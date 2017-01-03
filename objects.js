c71b9d14a71547c1908e39167cfb38c3b3b29902
George Higbie <gehigbie@gmail.com>
2017-01-03 16:59:45 -0700

    added more details to file

--- objects.js
+++ objects.js
@@ -1,1 +1,5 @@
 //everything in javascript that is not a primitive type is an objects
+//object is a value type, with key value pairs
+
+//when variables are used in objects they are called properties
+//when functions are used in objects they are called methods
