e438d47ffc94b911e896f8a732702487bd41885e
George Higbie <gehigbie@gmail.com>
2017-01-03 12:58:30 -0700

    Added string variables

---
+++ strings.js
@@ -0,0 +1,21 @@
+//string examples
+
+let name = 'Jack';
+console.log(name);
+
+name = "Jane";
+console.log(name);
+
+//This is a no no ---> name = "Bill' you can't mix quotes
+
+let message = "nice to see you";
+console.log(message);
+
+message = 'It\'s nice to see you!'; //this is using an escape character
+console.log(message);
+
+//any data type can be wrapped in quotes to be a String
+console.log(typeof 942);
+console.log(typeof "942");
+console.log(typeof true);
+console.log(typeof "true");
