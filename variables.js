376497946df41ca48275dbc5ca0cb2e00d147412
George Higbie <gehigbie@gmail.com>
2017-01-03 11:22:09 -0700

    initial commit

---
+++ variables.js
@@ -0,0 +1,14 @@
+var firstName = "Jack";
+var gradesToBeCountedInSchool = 5;
+
+var grades_to_be_counted_in_school = 5;
+
+var fullName = "Jack Davis"; //This is a String
+var itemsInCar = 32 // Number
+var isCartEmpty = false // Boolean
+
+var fistName = "Jack";
+var lastName = "Davis";
+
+console.log(firstName + " " + lastName); //ES5 way of concatenated
+console.log(firstName, lastName);
