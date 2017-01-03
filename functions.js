b82dc673f2382544ab7100e21010ec24ce5efe6f
George Higbie <gehigbie@gmail.com>
2017-01-03 15:45:02 -0700

    added comments to fucntions.js

---
+++ functions.js
@@ -0,0 +1,9 @@
+//Functions let you group a series of statements together to perform
+//some task. Once a function is created, it can be resused over and
+//over in your code. If you find yourselft repeating statments in your code,
+// then a function may be the answer to avoid repetition.
+//DRY - Don't repeat yourselft
+
+//Function in javascript are frist class objects...They can have properties
+//and methods just like any other object. Where they differ, is in the fact that
+// functions can be called.
