f00f64f66c68821f064e131e95860c1a9136525f
George Higbie <gehigbie@gmail.com>
2017-01-05 19:48:48 -0700

    added hotRod object

--- thisAndBind.js
+++ thisAndBind.js
@@ -1,1 +1,15 @@
 //This and Bind
+
+
+//The thing called 'this' is the object that 'owns' the javascript code
+
+let hotRod = {}; //new emtpty object
+
+hotRod = {
+  sound: 'vrooom',
+  soundOff: function(){
+    console.log(this.sound);
+  }
+}
+
+hotRod.soundOff();
