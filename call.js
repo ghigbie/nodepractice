d625aa7bfa7a54636fb083eec606ebf8ac62301a
George Higbie <gehigbie@gmail.com>
2017-01-05 20:20:36 -0700

    added more examples of call in use

--- call.js
+++ call.js
@@ -10,7 +10,27 @@ let addToThis = function(a, b, c){
   return this.num + a + b +c;
 };
 
+addToThis();
+
+
 //Call binds the function to the first object passed in.
 //Parameters are passed in subsequent values
 
 console.log(addToThis.call(obj, 1, 2, 3));
+
+let person1 = {
+  firstName: 'Jack',
+  lastName: 'Davis'
+};
+
+ let person2 = {
+   firstName: 'Mark',
+   lastName: 'Price'
+ };
+
+ function hello(greeting){
+   console.log(`${greeting} ${this.firstName} ${this.lastName}`);
+ }
+
+ hello.call(person1, "What\'s up?");
+ hello.call(person2, "Yo");
