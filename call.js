3a27f163bfae626e4e88106f03c1e5be62b27c4c
George Higbie <gehigbie@gmail.com>
2017-01-05 20:11:48 -0700

    applied call

--- call.js
+++ call.js
@@ -1,3 +1,16 @@
 //call
 
-// 
+
+let obj = {
+  num : 2
+};
+
+let addToThis = function(a, b, c){
+  //'this' has no context here
+  return this.num + a + b +c;
+};
+
+//Call binds the function to the first object passed in.
+//Parameters are passed in subsequent values
+
+console.log(addToThis.call(obj, 1, 2, 3));
